5bcdd81b0eec6d9bf501eba69495863b5bd30b6f
Garrett Matthew Forthofer <gf00642@gmail.com>
2022-03-18 16:35:56 +0000

    added roll and stat bonuses

--- PA5/Encounter.cpp
+++ PA5/Encounter.cpp
@@ -1,9 +1,14 @@
 #include<string>
 #include<map>
 #include<iostream>
+#include<random>
+#include<time.h>
 #include"Image.h"
 #include"Character.h"
 
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using std::chrono::system_clock;
 using namespace std;
 
 class Encounter {
@@ -28,4 +33,17 @@ Encounter::Encounter(Character& pc, Character& enemy, map<string, Image> IMAGES,
 		cout << UIS.find("CombatMenu.txt")->second.GetImage();
 
 	}
+}
+
+int GetBonus(int stat) {
+	return (stat - 10) / 2;
+}
+
+int Roll(int count, int d) {
+	srand(time(NULL));
+	int roll = 0;
+	for (int i = 0; i < count; i++) {
+		roll += rand() % d;
+	}
+	return roll;
 }
